0cfc487421ead920f0480b17e90dbc9b8127dc64
unknown <melanie.caro78@gmail.com>
2021-01-07 18:32:20 +0100

    update PostEditor component

--- client/src/components/PostEditor.tsx
+++ client/src/components/PostEditor.tsx
@@ -1,90 +1,37 @@
-import React from 'react';
-import * as Types from '../../../commons/Types';
-import { usePost } from '../hooks/usePost';
+import { useHPContent } from '../hooks/useHPContent';
 
-export class PostEditor extends React.Component<
-  Types.ICategoriesAndTagsProps,
-  Types.IPostEditorState
-> {
-  state = {
-    newPostData: {
-      newPostTitle: '',
-      newPostContent: '',
-      newPostImg: '',
-      newPostTags: [],
-      newPostCategories: [],
-      authorName: '',
-    },
-  };
-
-  render() {
-    const { allCategories, allTags } = this.props;
-    return (
-      <div>
-        <h1>Write a new article</h1>
-        <form action="" method="post">
-          <label htmlFor="postTitle">Enter the post title</label>
-          <input id="postTitle" type="text" name="postTitle" />
-          <label htmlFor="postContent">Content:</label>
-          <textarea
-            cols={30}
-            rows={10}
-            wrap="soft"
-            id="postContent"
-            name="postContent"
-          />
-          <label htmlFor="postCategories">Categories</label>
-          <input type="text" id="postCategories" list="categoriesList" />
-          <datalist id="categoriesList">
-            {allCategories &&
-              allCategories.map((category) => <option>{category}</option>)}
-          </datalist>
-
-          <label htmlFor="postTags">Tags</label>
-          <input type="text" id="postTags" list="TagsList" />
-          <datalist id="TagsList">
-            {allTags && allTags.map((tag) => <option>{tag}</option>)}
-          </datalist>
-          <label htmlFor="postImg">Add an image for your article</label>
-          <input type="file" name="postImg" id="postImg" />
-          <button type="submit">Publish</button>
-        </form>
-      </div>
-    );
-  }
-}
-
-const postEditor = () => {
+export const PostEditor = () => {
+  const { allCategories, allTags } = useHPContent();
   return (
-    // <div>
-    //   <h1>Write a new article</h1>
-    //   <form action="" method="post">
-    //     <label htmlFor="postTitle">Enter the post title</label>
-    //     <input id="postTitle" type="text" name="postTitle" />
-    //     <label htmlFor="postContent">Content:</label>
-    //     <textarea
-    //       cols={30}
-    //       rows={10}
-    //       wrap="soft"
-    //       id="postContent"
-    //       name="postContent"
-    //     />
-    //     <label htmlFor="postCategories">Categories</label>
-    //     <input type="text" id="postCategories" list="categoriesList" />
-    //     <datalist id="categoriesList">
-    //       {allCategories &&
-    //         allCategories.map((category) => <option>{category}</option>)}
-    //     </datalist>
+    <div>
+      <h1>Write a new article</h1>
+      <form action="" method="post">
+        <label htmlFor="postTitle">Enter the post title</label>
+        <input id="postTitle" type="text" name="postTitle" />
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          cols={30}
+          rows={10}
+          wrap="soft"
+          id="postContent"
+          name="postContent"
+        />
+        <label htmlFor="postCategories">Categories</label>
+        <input type="text" id="postCategories" list="categoriesList" />
+        <datalist id="categoriesList">
+          {allCategories &&
+            allCategories.map((category) => <option>{category}</option>)}
+        </datalist>
 
-    //     <label htmlFor="postTags">Tags</label>
-    //     <input type="text" id="postTags" list="TagsList" />
-    //     <datalist id="TagsList">
-    //       {allTags && allTags.map((tag) => <option>{tag}</option>)}
-    //     </datalist>
-    //     <label htmlFor="postImg">Add an image for your article</label>
-    //     <input type="file" name="postImg" id="postImg" />
-    //     <button type="submit">Publish</button>
-    //   </form>
-    // </div>
+        <label htmlFor="postTags">Tags</label>
+        <input type="text" id="postTags" list="TagsList" />
+        <datalist id="TagsList">
+          {allTags && allTags.map((tag) => <option>{tag}</option>)}
+        </datalist>
+        <label htmlFor="postImg">Add an image for your article</label>
+        <input type="file" name="postImg" id="postImg" />
+        <button type="submit">Publish</button>
+      </form>
+    </div>
   );
 };
